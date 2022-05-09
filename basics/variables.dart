67d3a31cb0fb8dde0939657cd5a1612bd0d008bc
hmmisthisgood <randomtrain2021@outlook.com>
2022-05-09 18:44:39 +0545

    learned dart variables

---
+++ basics/variables.dart
@@ -0,0 +1,59 @@
+main() {
+  var someVaraible = false;
+  // someVaraible = 500;
+
+  /// declaration
+  int balance;
+
+  /// definition
+  balance = 200;
+  // balance = 100.0;
+
+  // balance = "200";
+
+  /// numerical data types
+  /// int (1,32,-1)
+  /// double (decimal)= 0.01, 1.0
+  /// num = 1.0, 10, 11
+
+  double discount = 10.0;
+  discount = 100;
+
+  print(discount);
+  num number = 100;
+  number = 10.0;
+
+  /// String
+
+  String name = "Some Person";
+
+  /// bool
+
+  bool isSunday = false; // true, false
+
+  /// dynamic;
+  ///
+  dynamic thisIsDynamic = 1000;
+
+  print(thisIsDynamic);
+  thisIsDynamic = "5000";
+
+  // var sum = thisIsDynamic;
+  // print(sum);
+  thisIsDynamic = false;
+
+  print(thisIsDynamic);
+
+  /// const , final
+
+  const pi = 3.14;
+  // pi = 3.141528;
+
+  final int gravity = 10;
+  // gravity = 10;
+
+  final currentTime = DateTime.now();
+  // currentTime = DateTime.now();
+
+  print(currentTime);
+}
