efc5ee764ef77f59bdcbda706cf323679c9f9f0f
hmmisthisgood <randomtrain2021@outlook.com>
2022-05-17 18:58:22 +0545

    learned about list and map

---
+++ basics/list_and_map.dart
@@ -0,0 +1,46 @@
+main() {
+  /// List
+  /// Set
+  /// Map
+
+  var dyhnamicList = [2, 'Ram bahadur', 3, false, 3.4, 10.0];
+
+  /// exlplicit list definition
+////////////////////////// 0, 1 , 2,3
+  List<String> students = ["Ram", "Sita", "Ram", "Jack", "A1"];
+  print(students);
+
+  var first = students[0];
+  var last = students[3];
+
+  students.add("Jack");
+
+  // students.addAll(['jeff', "michael"]);
+
+  students.removeAt(0);
+
+  print(students);
+  students.toSet();
+
+  /// Set
+  ///
+  Set setStudents = {"Ram", "Sita", "Ram", "jack"};
+
+  print(setStudents);
+
+  /// Map
+
+  Map<String, dynamic> person = {
+    "age": 22,
+    "name": "Jeff", // MapEntry
+    "rollNo": 122,
+  };
+
+  var userAge = person["age"];
+  print(userAge);
+
+  person["age"] = 35;
+
+  person["birthday"] = "Oct 1";
+  print(person);
+}
