99d2bb92939ec12148d35ce6ea79d921922bfb51
hmmisthisgood <randomtrain2021@outlook.com>
2022-06-01 19:34:49 +0545

    single child scroll view used to make column scrollable

--- our_app/lib/screen/row_and_columns_screen.dart
+++ our_app/lib/screen/row_and_columns_screen.dart
@@ -10,49 +10,57 @@ class RowAndColumnScreen extends StatelessWidget {
       body: Container(
         // color: Colors.grey,
         width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            NewsCard(), //150
-            Container(
-                height: 150,
-                width: 200,
-                margin: EdgeInsets.all(20), //40
-                padding: EdgeInsets.all(20),
-                alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              NewsCard(), //150
+              NewsCard(),
+              Container(
+                  height: 150,
+                  width: 200,
+                  margin: EdgeInsets.all(20), //40
+                  padding: EdgeInsets.all(20),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(width: 1, color: Colors.red),
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.red.withOpacity(0.3),
+                            offset: Offset(3, 3),
+                            blurRadius: 10,
+                            spreadRadius: 2),
+                      ]
+                      // shape: BoxShape.circle,
+                      ),
+                  child: Text("Conainer")
+                  // Container(color: Colors.green),
+                  ),
+              Container(
+                height: 200,
+                width: 100,
                 decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(width: 1, color: Colors.red),
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.red.withOpacity(0.3),
-                          offset: Offset(3, 3),
-                          blurRadius: 10,
-                          spreadRadius: 2),
-                    ]
-                    // shape: BoxShape.circle,
-                    ),
-                child: Text("Conainer")
-                // Container(color: Colors.green),
+                  gradient: LinearGradient(
+                      colors: [Colors.blue, Colors.pink],
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter),
                 ),
-            Container(
-              height: 200,
-              width: 100,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors: [Colors.blue, Colors.pink],
-                    begin: Alignment.bottomCenter,
-                    end: Alignment.topCenter),
               ),
-            ),
-            // Container(height: 50, width: 30, color: Colors.purple),
-            // Container(height: 50, width: 30, color: Colors.pink),
-            // Container(height: 50, width: 30, color: Colors.black),
-            // Container(height: 50, width: 30, color: Colors.yellow),
-          ],
+
+              Container(
+                height: 500,
+                color: Colors.brown,
+              )
+              // Container(height: 50, width: 30, color: Colors.purple),
+              // Container(height: 50, width: 30, color: Colors.pink),
+              // Container(height: 50, width: 30, color: Colors.black),
+              // Container(height: 50, width: 30, color: Colors.yellow),
+            ],
+          ),
         ),
       ),
     );
