9bc68862aa9b13cf4aae8afb4139d46e4a6aa23d
hmmisthisgood <randomtrain2021@outlook.com>
2022-06-14 18:40:20 +0545

    json serialization learned

--- http_basics/lib/screen/homepage.dart
+++ http_basics/lib/screen/homepage.dart
@@ -5,6 +5,8 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as api;
 
+import '../model/post.dart';
+
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
 
@@ -14,6 +16,7 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   var dataFromServer = [];
+  List<Post> postsList = [];
   @override
   void initState() {
     print("1. init state called");
@@ -33,12 +36,54 @@ class _HomepageState extends State<Homepage> {
       print(res.statusCode);
 
       final List decoded = json.decode(res.body);
+
+      /// Map
+      /// map
+      final List<Post> converetedList = decoded.map<Post>((item) {
+        final convertedPost = Post.convertJsonToDart(item);
+        return convertedPost;
+      }).toList();
+      postsList = converetedList;
+
       print(decoded);
       dataFromServer = decoded;
       setState(() {});
     }).catchError((e) {
       print(e);
     });
+  }
+
+  postDataToServer() async {
+    print("posting data to server");
+
+    final url = "https://jsonplaceholder.typicode.com/posts/";
+    final uri = Uri.parse(url);
+
+    try {
+      final data = await api.post(uri, body: {
+        "userId": "12432",
+        "title": "This is a random post from us",
+        "body": "ohh this an awesomr post"
+      });
+
+      final secondData = await api.post(uri, body: data.body);
+    } catch (e, s) {
+////
+      // api.post(uri, body: {
+      //   "userId": "12432",
+      //   "title": "This is a random post from us",
+      //   "body": "ohh this an awesomr post"
+      // }).then((res) {
+      //   api.post(uri, body: res.body).then((e) {}).catchError((e) {});
+
+      //   print("posted dat to server success!");
+      //   print(res.statusCode);
+      //   print(res.body);
+      // }).catchError((e, s) {
+      //   print(e);
+      //   print(s);
+      // });
+    }
   }
 
   @override
@@ -54,26 +99,36 @@ class _HomepageState extends State<Homepage> {
 
     return Scaffold(
       body: ListView.builder(
-        itemCount: dataFromServer.length, //100
+        itemCount: postsList.length, //100
         itemBuilder: (context, index) {
-          final item = dataFromServer[index];
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              color: Colors.grey.withOpacity(0.3),
+          // final item = dataFromServer[index];
+          final item = postsList[index];
+          return InkWell(
+            onTap: () {
+              postDataToServer();
+            },
+            child: Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    item["title"],
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(item["body"]),
-                ],
+              child: Container(
+                color: Colors.grey.withOpacity(0.3),
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      // item["title"],
+                      item.title,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                        // item["body"],
+                        item.body),
+                  ],
+                ),
               ),
             ),
           );
